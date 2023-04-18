61b4a4da6cb647c2f7b8a91b017102216e5eb763
Mohammed Aljezawi <killerm250m@gmail.com>
2023-04-18 17:48:08 +0300

    Update IsolateHolderService.kt
    try to fix race condition

--- android/src/main/kotlin/yukams/app/background_locator_2/IsolateHolderService.kt
+++ android/src/main/kotlin/yukams/app/background_locator_2/IsolateHolderService.kt
@@ -95,10 +95,8 @@ class IsolateHolderService : MethodChannel.MethodCallHandler, LocationUpdateList
         // Starting Service as foreground with a notification prevent service from closing
         val notification = getNotification()
         startForeground(notificationId, notification)
-
-        pluggables.forEach {
-            context?.let { it1 -> it.onServiceStart(it1) }
-        }
+        
+        Handler(Looper.getMainLooper()).postDelayed( { pluggables.forEach { context?.let { it1 -> it.onServiceStart(it1) } } }, 1000  )
     }
 
     private fun getNotification(): Notification {
